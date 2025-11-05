7c4e909c2711d49bde70567f45f426692f88a012
owen151128 <owen151128@gmail.com>
2025-11-06 07:52:39 +0900

    feat(appbar): Add instagram app bar

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,6 +20,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(CupertinoIcons.camera, color: Colors.black),
+          onPressed: () {},
+        ),
+        title: Image.asset("assets/logo.png", height: 32),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: const Icon(CupertinoIcons.paperplane, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      ),
       body: const Center(child: Text("Hello HomePage!")),
     );
   }
