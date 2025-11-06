dc96423623d04430f076b3777374eb5a380f223e
owen151128 <owen151128@gmail.com>
2025-11-06 15:18:55 +0900

    feat(home): Add miso home page

--- miso/lib/home_page.dart
+++ miso/lib/home_page.dart
@@ -5,10 +5,67 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: Center(
-      child: Text(
-        "Home!",
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+    backgroundColor: Theme.of(context).primaryColor,
+    body: SizedBox(
+      width: double.infinity,
+      child: Column(
+        children: [
+          SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+          Text(
+            "대한민국 1등 홈서비스\n미소를 만나보세요!",
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 38),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(64),
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.add, color: Theme.of(context).primaryColor),
+                  SizedBox(width: 6),
+                  Text(
+                    "예약하기",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Spacer(),
+          FilledButton(
+            onPressed: () {},
+            style: FilledButton.styleFrom(
+              backgroundColor: Colors.white.withValues(alpha: 0.3),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+            ),
+            child: Text(
+              "서비스 상세정보",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+        ],
       ),
     ),
   );
