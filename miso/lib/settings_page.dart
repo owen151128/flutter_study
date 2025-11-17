8e34037f044c91f57d879ae78efd16ab98c93fe6
owen151128 <owen151128@gmail.com>
2025-11-17 14:08:05 +0900

    feat(settings): Add miso settings page

--- miso/lib/settings_page.dart
+++ miso/lib/settings_page.dart
@@ -5,10 +5,66 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: Center(
-      child: Text(
-        "Settings!",
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+    body: SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.10),
+              Text(
+                textAlign: TextAlign.start,
+                "나의 정보",
+                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 32),
+              ),
+              SizedBox(height: 16),
+              Text(
+                textAlign: TextAlign.start,
+                "010-1234-5678",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 32),
+              ListTile(
+                onTap: () => {},
+                contentPadding: EdgeInsets.all(0),
+                leading: Icon(Icons.home_outlined, color: Colors.black),
+                title: Text(
+                  "주소 관리",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              ListTile(
+                onTap: () => {},
+                contentPadding: EdgeInsets.all(0),
+                leading: Icon(Icons.credit_card_outlined, color: Colors.black),
+                title: Text(
+                  "결제 수단 관리",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              ListTile(
+                onTap: () => {},
+                contentPadding: EdgeInsets.all(0),
+                leading: Icon(Icons.article_outlined, color: Colors.black),
+                title: Text(
+                  "공지사항",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              ListTile(
+                onTap: () => {},
+                contentPadding: EdgeInsets.all(0),
+                leading: Icon(Icons.help_outline, color: Colors.black),
+                title: Text(
+                  "문의사항",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
