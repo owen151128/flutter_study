f4f771fbcf8dbb89638deb5a0dd4b5805f5d37df
owen151128 <owen151128@gmail.com>
2025-11-17 18:27:33 +0900

    feat(click): Add starbucks order page click event

--- starbucks/lib/order/menu_item.dart
+++ starbucks/lib/order/menu_item.dart
@@ -13,35 +13,38 @@ class MenuItem extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => Padding(
-    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 21),
-    child: Row(
-      children: [
-        CircleAvatar(
-          radius: 52,
-          backgroundImage: NetworkImage(imageUrl),
-          backgroundColor: Colors.transparent,
-        ),
-        SizedBox(width: 16),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 21,
-                fontWeight: FontWeight.bold,
+  Widget build(BuildContext context) => InkWell(
+    onTap: () => {},
+    child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 21),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 52,
+            backgroundImage: NetworkImage(imageUrl),
+            backgroundColor: Colors.transparent,
+          ),
+          SizedBox(width: 16),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 21,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 6),
-            Text(
-              description,
-              style: TextStyle(color: Colors.grey, fontSize: 16),
-            ),
-          ],
-        ),
-      ],
+              SizedBox(height: 6),
+              Text(
+                description,
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+            ],
+          ),
+        ],
+      ),
     ),
   );
 }
