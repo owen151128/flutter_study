992d2968bd26cc1112383a111657c0050b18725f
owen151128 <owen151128@gmail.com>
2025-11-19 10:16:58 +0900

    feat(crud): Add bucket list app crud

--- bucket_list/lib/create/create_page.dart
+++ bucket_list/lib/create/create_page.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CreatePage extends StatelessWidget {
+class CreatePage extends StatefulWidget {
   const CreatePage({super.key});
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  TextEditingController textEditingController = TextEditingController();
+  String? error;
 
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
       title: Text("버킷리스트 작성"),
-      // 뒤로가기 버튼
-      leading: IconButton(
-        icon: Icon(CupertinoIcons.chevron_back),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      ),
     ),
     body: Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: textEditingController,
             autofocus: true,
-            decoration: InputDecoration(hintText: "하고 싶은 일을 입력하세요"),
+            decoration: InputDecoration(hintText: "하고 싶은 일을 입력하세요", errorText: error),
           ),
           SizedBox(height: 32),
           SizedBox(
@@ -31,6 +33,13 @@ class CreatePage extends StatelessWidget {
             child: ElevatedButton(
               child: Text("추가하기", style: TextStyle(fontSize: 18)),
               onPressed: () {
+                String job = textEditingController.text;
+                if (job.isNotEmpty) {
+                  setState(() => error = null);
+                  Navigator.pop(context, job);
+                } else {
+                  setState(() => error = "내용을 입력해주세요.");
+                }
               },
             ),
           ),
--- bucket_list/lib/home/home_page.dart
+++ bucket_list/lib/home/home_page.dart
@@ -1,21 +1,87 @@
 import 'package:bucket_list/create/create_page.dart';
+import 'package:bucket_list/model/bucket.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Bucket> bucketList = [];
 
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(title: Text("버킷 리스트")),
-    body: Center(child: Text("버킷 리스트 를 작성해 주세요.")),
+    body: bucketList.isEmpty
+        ? Center(child: Text("버킷 리스트를 작성해 주세요."))
+        : ListView.builder(
+            itemCount: bucketList.length,
+            itemBuilder: (context, index) {
+              Bucket bucket = bucketList[index];
+              return ListTile(
+                title: Text(
+                  bucket.job,
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: bucket.isDone ? Colors.grey : Colors.black,
+                    decoration: bucket.isDone
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none,
+                  ),
+                ),
+                trailing: IconButton(
+                  icon: Icon(CupertinoIcons.delete),
+                  onPressed: () {
+                    showDeleteDialog(context, index);
+                  },
+                ),
+                onTap: () {
+                  setState(() => bucket.isDone = !bucket.isDone);
+                },
+              );
+            },
+          ),
     floatingActionButton: FloatingActionButton(
       child: Icon(Icons.add),
-      onPressed: () {
-        Navigator.push(
+      onPressed: () async {
+        String? job = await Navigator.push(
           context,
           MaterialPageRoute(builder: (_) => CreatePage()),
         );
+        if (job != null) {
+          setState(() => bucketList.add(Bucket(job, false)));
+        }
       },
     ),
   );
+
+  void showDeleteDialog(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("정말로 삭제하시겠습니까?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("취소"),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() => bucketList.removeAt(index));
+                Navigator.pop(context);
+              },
+              child: Text("확인", style: TextStyle(color: Colors.pink)),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
