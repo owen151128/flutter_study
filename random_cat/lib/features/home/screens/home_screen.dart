864a0d1c081a9b2579e687279242db3d58ebe7db
owen151128 <owen151128@gmail.com>
2025-11-24 18:02:11 +0900

    feat(cat): Add random cat project base

--- random_cat/lib/features/home/screens/home_screen.dart
+++ random_cat/lib/features/home/screens/home_screen.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:random_cat/features/favorite/screens/favorite_page.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(centerTitle: true, title: Text("Home")),
-    body: Center(
-      child: Text(
-        "Hello!",
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+    appBar: AppBar(
+      centerTitle: true,
+      foregroundColor: Colors.white,
+      title: Text("랜덤 고양이"),
+      backgroundColor: Colors.amber,
+      actions: [
+        IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => FavoritePage()),
+          ),
+        ),
+      ],
+    ),
+    body: GridView.count(
+      crossAxisSpacing: 8,
+      mainAxisSpacing: 8,
+      padding: EdgeInsets.all(8),
+      crossAxisCount: 2,
+      children: List.generate(
+        10,
+        (index) =>
+            Center(child: Text("$index", style: TextStyle(fontSize: 24))),
       ),
     ),
   );
--- random_cat/lib/main.dart
+++ random_cat/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:random_cat/random_cat_app.dart';
 
 void main() {
-  runApp(const RandomCatApp());
+  runApp(ProviderScope(child: const RandomCatApp()));
 }
